b26eb3fa5ba2e96283b772b4ed5d68ab26ed74bc
Francisco Miamoto <fsmiamoto@gmail.com>
2021-08-28 21:44:44 -0300

    feat(track): tune the pid gains

--- src/Projects/vehicle-controller/src/track_manager.c
+++ src/Projects/vehicle-controller/src/track_manager.c
@@ -7,9 +7,9 @@
 #include "utils/ustdlib.h"
 #include <stdio.h>
 
-#define Kp 6
-#define Ki 1
-#define Kd 10
+#define Kp 4.0
+#define Ki 2.4
+#define Kd 12.0
 
 void TrackManager(void *arg) {
   track_manager_t *t = (track_manager_t *)arg;
@@ -18,7 +18,7 @@ void TrackManager(void *arg) {
   track_manager_msg_t reading;
 
   float turn, error, last_error;
-  float abs_error, ref = 0.0, last_ref = 0.0;
+  float steering = 0.0, last_steering = 0.0;
   float derivative, integral = 0.0;
 
   char buffer[BUFFER_SIZE];
@@ -36,24 +36,20 @@ void TrackManager(void *arg) {
     osMessageQueueGet(t->args.qid, &reading, MSG_PRIO, osWaitForever);
 
     error = t->args.reference - reading.rf_reading;
-    abs_error = error > 0 ? error : -error;
-
-    if (abs_error < 0.01) {
-      error = 0;
-    }
 
     // PID Control
     derivative = error - last_error;
     integral += error;
-    ref = Kp * error + Ki * integral + Kd * derivative;
 
-    turn = ref - last_ref;
+    steering = Kp * error + Ki * integral + Kd * derivative;
+
+    turn = steering - last_steering;
 
     sprintf(buffer, ";V%f;", turn);
     turnRequest.content = buffer;
     osMessageQueuePut(writer.args.qid, &turnRequest, MSG_PRIO, osWaitForever);
 
     last_error = error;
-    last_ref = ref;
+    last_steering = steering;
   }
 }
