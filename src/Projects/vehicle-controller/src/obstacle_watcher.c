22b9537c141ec109778839569bc12b9078814d85
Francisco Miamoto <fsmiamoto@gmail.com>
2021-08-30 08:19:44 -0300

    feat(track): tune pid

--- src/Projects/vehicle-controller/src/obstacle_watcher.c
+++ src/Projects/vehicle-controller/src/obstacle_watcher.c
@@ -2,8 +2,6 @@
 #include "tasks.h"
 #include "utils.h"
 
-#define TIMEOUT 200
-
 static void returnToCenter(void *arg) {
   track.args.reference = TRACK_CENTER_REFERENCE;
 }
@@ -12,8 +10,7 @@ void ObstacleWatcher(void *arg) {
   obstacle_watcher_t *o = (obstacle_watcher_t *)arg;
   obstacle_watcher_msg_t reading;
   uart_writer_msg_t readingRequest = {.content = ";Pu;"};
-  
-  
+
   osTimerId_t timer = osTimerNew(returnToCenter, osTimerOnce, NULL, NULL);
 
   printThreadInit(o->tid);
@@ -23,18 +20,20 @@ void ObstacleWatcher(void *arg) {
 
     osMessageQueuePut(writer.args.qid, &readingRequest, MSG_PRIO,
                       osWaitForever);
-    osStatus_t status =
-        osMessageQueueGet(o->args.qid, &reading, MSG_PRIO, TIMEOUT);
+    osStatus_t status = osMessageQueueGet(o->args.qid, &reading, MSG_PRIO,
+                                          OBSTACLE_WATCHER_READ_TIMEOUT);
     if (status != osOK) {
       continue;
     }
 
-    if(reading.sensor_reading < 0) continue;
-    
-    if (reading.sensor_reading <= OBSTACLE_WARNING_DISTANCE && track.args.reference != TRACK_LEFT_REFERENCE) {
+    if (reading.sensor_reading < 0)
+      continue;
+
+    if (reading.sensor_reading <= OBSTACLE_WARNING_DISTANCE &&
+        track.args.reference != TRACK_LEFT_REFERENCE) {
       track.args.reference = TRACK_LEFT_REFERENCE;
-      osTimerStart(timer,
-                   SECOND*OBSTACLE_WARNING_DISTANCE / speed_ctl.args.target_speed);
+      osTimerStart(timer, SECOND * OBSTACLE_WARNING_DISTANCE /
+                              speed_ctl.args.target_speed);
     }
   }
 }
